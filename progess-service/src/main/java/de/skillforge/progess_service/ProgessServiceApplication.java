23076fb5b3ca4561ddc40b6117a3ce78b5c3370f
Jangleur1991 <Jangleur@web.de>
2025-10-15 09:44:17 +0100

    init project

---
+++ progess-service/src/main/java/de/skillforge/progess_service/ProgessServiceApplication.java
@@ -0,0 +1,13 @@
+package de.skillforge.progess_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgessServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgessServiceApplication.class, args);
+	}
+
+}
---
+++ progess-service/src/test/java/de/skillforge/progess_service/ProgessServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.skillforge.progess_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgessServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
